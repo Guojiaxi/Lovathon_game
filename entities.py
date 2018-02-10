bfa4da919908ce95d3b9fe42089d69fda9f9dfb1
Guojiaxi <joshliguo@gmail.com>
2018-02-10 15:31:55 -0800

    fuckme

--- entities.py
+++ entities.py
@@ -41,11 +41,23 @@ class Player(pygame.sprite.Sprite):
     def shoot(self,index,key,current_position,bullets):
         bullets.append(PlayerBullet(index,start_pos=(current_position[0]-3,current_position[1]-30), speed=15))
 
+    def is_hit(self):
+        for thing in bullets:
+            if self.box.colliderect(thing.hitbox):
+                del bullets[bullets.index(thing)]
+                # player needs to die or lose a life
+                # explosion animation
+
 class Enemy(object):
     def __init__(self, **kwargs):
         self.box = pygame.Rect(kwargs.get("start_pos",(width/2,0)),kwargs.get("size",(50,50)))
-        enemies.append
+        enemies.append(self)
 
+    def is_hit(self):
+        for thing in bullets:
+            if thing is PlayerBullet and self.box.colliderect(thing.hitbox):
+                enemies.remove(self)
+                bullets.remove(thing)
   
 class Bullet(pygame.sprite.Sprite):
     def __init__(self,**kwargs):
@@ -70,5 +82,11 @@ class PlayerBullet(Bullet):
             bullets.remove(self)
 
 class EnemyBullet(Bullet):
-    def __init__(self):
-        super(Bullet,self).__init__()
+    def __init__(self,index,**kwargs):
+        Bullet.__init__(self,**kwargs)
+
+    def move(self):
+        if self.rect.bottom >= 0:
+            self.rect.move_ip(0,-self.speed)
+        else:
+            bullets.remove(self)
