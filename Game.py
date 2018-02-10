c6aeec1587bcd6071a618bd277442ce70796beb5
Guojiaxi <33165607+Guojiaxi@users.noreply.github.com>
2018-02-10 12:16:33 -0800

    Update Game.py

--- Game.py
+++ Game.py
@@ -13,13 +13,10 @@ pygame.display.set_caption("game")
 clock = pygame.time.Clock()
 player = Player()
 
-speed = 5
-
 while running:
 
     clock.tick(60)
-
-    screen.fill(white)
+    screen.fillplayer.move(key)(white)
     pygame.draw.rect(screen, black, player.box, 0)
 
     for event in pygame.event.get():
@@ -28,21 +25,8 @@ while running:
 
     # basic movement
     key = pygame.key.get_pressed()
-    if key[pygame.K_LEFT]:
-        if player.box.left >= 0:
-            player.move(-speed,0)
-
-    if key[pygame.K_RIGHT]:
-        if player.box.right <= width:
-            player.move(speed,0)
-
-    if key[pygame.K_UP]:
-        if player.box.top >= 0:
-            player.move(0,-speed)
-
-    if key[pygame.K_DOWN]:
-        if player.box.bottom <= height:
-            player.move(0,speed)
+   
+    player.move(key)
 
     #draw
     pygame.display.flip()
