05084d1fb6dc955d3f1a9156d363524c098455b0
Guojiaxi <33165607+Guojiaxi@users.noreply.github.com>
2018-02-10 12:18:22 -0800

    Update entities.py

--- entities.py
+++ entities.py
@@ -3,14 +3,33 @@ from globalvars import *
 class Player(object):
     def __init__(self, **kwargs):
         self.box = pygame.Rect(width/2, height/2, kwargs.get("width",50), kwargs.get("height",50))
+        
+    def move(self,key):
+        if key[pygame.K_LSHIFT]:
+            self.speed = 3
+        else:
+            self.speed = 6
 
-    def move(self, dx, dy):
-        if dx != 0 or dy != 0:
-            self.move_xy(dx, dy)
+        if key[pygame.K_LEFT]:
+            if self.box.left >= 0:
+                self.move_xy(-self.speed, 0)
+
+        if key[pygame.K_RIGHT]:
+            if self.box.right <= width:
+                self.move_xy(self.speed, 0)
+
+        if key[pygame.K_UP]:
+            if self.box.top >= 0:
+                self.move_xy(0, -self.speed)
+
+        if key[pygame.K_DOWN]:
+            if self.box.bottom <= height:
+                self.move_xy(0, self.speed)
 
     def move_xy(self, dx, dy):
-        self.box.move_ip(dx,0)
-        self.box.move_ip(0,dy)
+        if dx != 0 or dy != 0:
+            self.box.move_ip(dx, 0)
+            self.box.move_ip(0, dy)
 
 class Enemy(object):
     def __init__(self, **kwargs):
